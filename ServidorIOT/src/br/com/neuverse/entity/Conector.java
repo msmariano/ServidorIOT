ad63e19838b524ccd19f1442d61489719ff764c1
msmariano <marcelodossantosmariano@gmail.com>
2020-08-21 15:02:43 -0300

    versao inicial

---
+++ ServidorIOT/src/br/com/neuverse/entity/Conector.java
@@ -0,0 +1,78 @@
+package br.com.neuverse.entity;
+
+import br.com.neuverse.enumerador.Status;
+import br.com.neuverse.enumerador.TipoIOT;
+
+public class Conector {
+	private String id;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public Iot getIot() {
+		return iot;
+	}
+	public void setIot(Iot iot) {
+		this.iot = iot;
+	}
+	public TipoIOT getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoIOT tipo) {
+		this.tipo = tipo;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public String getErro() {
+		return erro;
+	}
+	public void setErro(String erro) {
+		this.erro = erro;
+	}
+	public ControllerIot getControlerIot() {
+		return controlerIot;
+	}
+	public void setControlerIot(ControllerIot controlerIot) {
+		this.controlerIot = controlerIot;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	private String usuario;
+	private String senha;
+	private String ip;
+	private String nome;
+	private Iot iot;
+	private ControllerIot controlerIot;
+	private TipoIOT tipo;
+	private Status status;
+	private String erro;
+		
+}
---
+++ ServidorIOT/src/br/com/neuverse/principal/Main.java
@@ -0,0 +1,64 @@
+package br.com.neuverse.principal;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import br.com.neuverse.entity.ButtonIot;
+import br.com.neuverse.entity.Conector;
+import br.com.neuverse.entity.Iot;
+import br.com.neuverse.enumerador.Status;
+import br.com.neuverse.enumerador.TipoIOT;
+
+public class Main {
+	
+	private static ServerSocket servidor;
+	private List<Conector> listaConectores;
+	private List<Cliente> clientes;
+	final Integer serverPort = 27015;
+	
+	public static void main(String[] args) throws IOException {
+		Main mainServidor = new Main();
+		Log.grava("Bem vindo ao Servidor IOT!");
+		servidor  = new ServerSocket(mainServidor.serverPort);
+		Log.grava(String.valueOf(mainServidor.serverPort));
+		mainServidor.listaConectores = new ArrayList<>();
+		mainServidor.clientes = new ArrayList<>();
+		System.out.println("Versao 1.0.0");
+		
+		
+		new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Socket socketCliente;
+						socketCliente = servidor.accept();
+						Log.grava("Cliente conectado: " + socketCliente.getInetAddress().getHostAddress() + " porta "
+								+ socketCliente.getPort());
+						new Thread() {
+							@Override
+							public void run() {
+								Cliente cliente = new Cliente();
+								mainServidor.clientes.add(cliente);
+								cliente.setSocketCliente(socketCliente);
+								cliente.setListaConectores(mainServidor.listaConectores);
+								cliente.setClientes(mainServidor.clientes);
+								cliente.run();
+							}
+						}.start();
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start();
+	}
+}
