41e17e88a16a0bc4510470ce92d50da2d655c84b
MARCELO DOS SANTOS MARIANO <marcelodossantosmariano@gmail.com>
2022-10-27 08:25:52 -0300

    .

--- src/br/com/neuverse/entity/Versao.java
+++ src/br/com/neuverse/entity/Versao.java
@@ -3,8 +3,8 @@ package br.com.neuverse.entity;
 import java.util.Date;
 
 public class Versao {
-    static final String versao = "1.0.13";
-    static final String data = "22/10/2022 21:00";
+    static final String versao = "1.0.14";
+    static final String data = "27/10/2022 09:00";
     public Versao(){
         upDate = new Date();
     }
